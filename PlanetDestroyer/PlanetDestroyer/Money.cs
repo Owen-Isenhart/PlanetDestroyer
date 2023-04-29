4228b3f7f4584ac250483a434e6b072913eddc08
Owen Isenhart <Isenhart.owen@gmail.com>
2023-04-29 14:55:28 -0500

    Add project files.

---
+++ PlanetDestroyer/PlanetDestroyer/Money.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlanetDestroyer
+{
+    class Money
+    {
+        public void Update()
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
